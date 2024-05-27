d6e5ef09d666be6ed682e096ec541bc1fdd651fe
seola1ne <seolalny@gmail.com>
2024-05-27 14:59:08 +0900

    set : 프로젝트 세팅

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,9 @@
+
+export default function landing() {
+    return (
+      <>
+        랜딩 페이지
+      </>
+    )
+  }
+  
---
+++ src/styles/global/index.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'NanumSquareRound';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    
+    * {
+        font-family: 'NanumSquareRound';
+    }
+
+    body {
+        margin: 0;
+    }
+
+    input {
+        outline: none;
+    }
+
+    button {
+        outline: none;
+    }
+
+    p {
+        margin: 0;
+    }
+`;
